a71ffd53a3365f59c1976aa9eea30c879059f676
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-08-28 09:57:20 +0000

    fin?

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,34 +1,50 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
 export const Navbar = () => {
-const {store, actions} = useContext(Context)
-console.log(store.favoritos)
+	const { store, actions } = useContext(Context)
 	return (
-		<div className="barra_principal">
-			<nav className="navbar">
-				<div className="barra">
-					<a className="navbar-brand" href="#">
-						<Link to={"./"} style={{ textDecoration: "none" }}>
-							<img className="logo" src="https://pngimg.com/d/star_wars_logo_PNG6.png" alt="logo" width="30" height="24" />
-						</Link>
-					</a>
-					<button type="button" className="favoritos btn btn-warning">Favorites</button>
-				</div>
-				<div>
-					{
-						store.favoritos.length > 0 ? 
-						store.favoritos.map((favorito) => {
-							return (
-								<p style={{color:"white"}}>{favorito}<i className="fa-solid fa-trash" onClick={()=>{actions.eliminarFavorito(favorito)}}></i></p>
-							)
-						}):
-						<p>empty</p>
-					}
-				</div>
-			</nav>
-		</div>
-	);
-};
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+			<div className="container-fluid">
+				<a className="navbar-logo" href="#">
+					<Link to={"./"} style={{ textDecoration: "none" }}>
+						<img className="logo" src="https://pngimg.com/d/star_wars_logo_PNG6.png" alt="logo" width="30" height="24" />
+					</Link>
+				</a>
+			</div>
+			<button className="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Favorites ({store.favoritos.length})
+                    </button>
+                    <ul className="dropdown-menu dropdown-menu-end">
+                        {
+                            store.favoritos.length > 0 ? (
+                                store.favoritos.map((favorito, index) => (
+                                    <li key={index}>
+                                        <a className="dropdown-item" href="#">
+                                            {favorito}
+                                            <i
+                                                className="fa-solid fa-trash"
+                                                onClick={() => actions.eliminarFavorito(favorito)}
+                                                style={{ marginLeft: '10px', cursor: 'pointer' }}
+                                            ></i>
+                                        </a>
+                                    </li>
+                                ))
+                            ) : (
+                                <p className="dropdown-item">You don't have favorites</p>
+                            )
+                        }
+                    </ul>
+	</nav >
+
+
+
+
+
+
+
+
+
+	)}
